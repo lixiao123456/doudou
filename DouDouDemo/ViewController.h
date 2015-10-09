2dd9c686a142f19efe4e371567d20b4578331722
xiao817287 <li817287@gmail.com>
2015-10-09 14:10:48 +0800

    Initial Commit

---
+++ DouDouDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DouDouDemo
+//
+//  Created by lixiao on 15/10/9.
+//  Copyright (c) 2015年 lixiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DouDouDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DouDouDemo
+//
+//  Created by lixiao on 15/10/9.
+//  Copyright (c) 2015年 lixiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
